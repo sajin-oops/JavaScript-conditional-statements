e5ab2722462939a837d8bd253ddfc08558e97735
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2023-10-10 17:16:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let name = "sajin"
+let age =23
+if(age)
+{
+  console.log(`Hello ${name}, Welcome back!!`)
+if (age >=18) {
+  console.log("Your eligible for the youth games" )
+} if (age >=20) 
+{
+  console.log("no.1 Dargon game")
+} 
+}
+else{
+  console.log("You are not eligible")
+}
